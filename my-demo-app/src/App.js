cdf2da6ec0bd69e9a4aabece6a357c6f18de2c1a
Nikhil-shah <nikshah41@gmail.com>
2021-07-31 19:14:23 +0530

    files under src added

---
+++ my-demo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CustomDataTable from './component/datatable/CustomDataTable';
+import Navbar from './component/navbar/Navbar'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <CustomDataTable/>
+    </div>
+  );
+}
+
+export default App;
