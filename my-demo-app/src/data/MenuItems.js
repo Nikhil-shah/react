6dde09cc1a8a7f415e84d12e48d2e0b36a2b5f1c
Nikhil-shah <nikshah41@gmail.com>
2021-07-31 19:16:00 +0530

    data files

---
+++ my-demo-app/src/data/MenuItems.js
@@ -0,0 +1,19 @@
+export const MenuItems = [
+    {
+        title: 'Products',
+        url: '#',
+        cName: 'nav-links'
+    },{
+        title: 'Serives',
+        url: '#',
+        cName: 'nav-links'
+    },{
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    }
+];
+
+export const HomeSubMenuItems = ['Home 1', 'Home 2', 'Home 3'];
+export const ProductSubMenuItems = ['Product 1', 'Product 2', 'Product 3'];
+export const ServiceSubMenuItems = ['Service 1', 'Service 2', 'Service 3'];
--- my-demo-app/src/data/data.js
+++ my-demo-app/src/data/data.js
@@ -1,1 +1,151 @@
-
+export const customers = [
+      {
+        "id": 1000,
+        "name": "James Butt",
+        "company": "Benton, John B Jr",
+        "date": "2015-09-13",
+        "status": "unqualified",
+        "verified": true,
+        "activity": 17,
+        "balance": 70663
+      },
+      {
+        "id": 1001,
+        "name": "Josephine Darakjy",
+        "company": "Chanay, Jeffrey A Esq",
+        "date": "2019-02-09",
+        "status": "proposal",
+        "verified": true,
+        "activity": 0,
+        "balance": 82429
+      },
+      {
+        "id": 1002,
+        "name": "Art Venere",
+        "company": "Chemel, James L Cpa",
+        "date": "2017-05-13",
+        "status": "qualified",
+        "verified": false,
+        "activity": 63,
+        "balance": 28334
+      },
+      {
+        "id": 1003,
+        "name": "Lenna Paprocki",
+        "company": "Feltz Printing Service",
+        "date": "2020-09-15",
+        "status": "new",
+        "verified": false,
+        "activity": 37,
+        "balance": 88521
+      },
+      {
+        "id": 1004,
+        "name": "Donette Foller",
+        "company": "Printing Dimensions",
+        "date": "2016-05-20",
+        "status": "proposal",
+        "verified": true,
+        "activity": 33,
+        "balance": 93905
+      },
+      {
+        "id": 1005,
+        "name": "Simona Morasca",
+        "company": "Chapman, Ross E Esq",
+        "date": "2018-02-16",
+        "status": "qualified",
+        "verified": false,
+        "activity": 68,
+        "balance": 50041
+      },
+      {
+        "id": 1006,
+        "name": "Mitsue Tollner",
+        "company": "Morlong Associates",
+        "date": "2018-02-19",
+        "status": "renewal",
+        "verified": true,
+        "activity": 54,
+        "balance": 58706
+      },
+      {
+        "id": 1007,
+        "name": "Leota Dilliard",
+        "company": "Commercial Press",
+        "date": "2019-08-13",
+        "status": "renewal",
+        "verified": true,
+        "activity": 69,
+        "balance": 26640
+      },
+      {
+        "id": 1008,
+        "name": "Sage Wieser",
+        "company": "Truhlar And Truhlar Attys",
+        "date": "2018-11-21",
+        "status": "unqualified",
+        "verified": true,
+        "activity": 76,
+        "balance": 65369
+      },
+      {
+        "id": 1009,
+        "name": "Kris Marrier",
+        "company": "King, Christopher A Esq",
+        "date": "2015-07-07",
+        "status": "proposal",
+        "verified": false,
+        "activity": 3,
+        "balance": 63451
+      },
+      {
+        "id": 1010,
+        "name": "Minna Amigon",
+        "company": "Dorl, James J Esq",
+        "date": "2018-11-07",
+        "status": "qualified",
+        "verified": false,
+        "activity": 38,
+        "balance": 71169
+      },
+      {
+        "id": 1011,
+        "name": "Abel Maclead",
+        "company": "Rangoni Of Florence",
+        "date": "2017-03-11",
+        "status": "qualified",
+        "verified": true,
+        "activity": 87,
+        "balance": 96842
+      },
+      {
+        "id": 1012,
+        "name": "Kiley Caldarera",
+        "company": "Feiner Bros",
+        "date": "2015-10-20",
+        "status": "unqualified",
+        "verified": false,
+        "activity": 80,
+        "balance": 92734
+      },
+      {
+        "id": 1013,
+        "name": "Graciela Ruta",
+        "company": "Buckley Miller & Wright",
+        "date": "2016-07-25",
+        "status": "negotiation",
+        "verified": false,
+        "activity": 59,
+        "balance": 45250
+      },
+      {
+        "id": 1014,
+        "name": "Cammy Albares",
+        "company": "Rousseaux, Michael Esq",
+        "date": "2019-06-25",
+        "status": "new",
+        "verified": true,
+        "activity": 90,
+        "balance": 30236
+      }];
